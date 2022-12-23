6ec3ff6c35b6ca25351bd778907f4d28180dce65
Dmitry <2dtwod@gmail.com>
2022-12-23 16:32:23 +0500

    development

--- src/main/java/org/goznak/controllers/Search.java
+++ src/main/java/org/goznak/controllers/Search.java
@@ -1,15 +1,21 @@
 package org.goznak.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.goznak.models.System;
 import org.goznak.services.SystemService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/search")
 public class Search {
+    final
+    int NUM_OF_VISIBLE_ROWS = 2;
     final
     SystemService systemService;
 
@@ -22,8 +28,32 @@ public class Search {
         return "search";
     }
     @GetMapping("/systems")
-    public String systemSearch(Model model){
-        model.addAttribute("systems", systemService.findAll());
+    @SuppressWarnings("unchecked")
+    public String systemSearch(Model model, HttpSession session, HttpServletRequest request){
+        int page;
+        List<System> systems;
+        String pageStr = request.getParameter("page");
+        if(pageStr == null){
+            systems = systemService.findAll();
+            session.setAttribute("systems", systems);
+            page = 1;
+        } else {
+
+            systems = (List<System>) session.getAttribute("systems");
+            if(systems == null){
+                return "redirect:/search/systems";
+            }
+            page = Integer.parseInt(pageStr);
+        }
+        int numOfPages = (int) Math.round((double) systems.size() / NUM_OF_VISIBLE_ROWS);
+        page = Math.max(page, 1);
+        page = Math.min(page, numOfPages);
+        int offset = (page - 1) * NUM_OF_VISIBLE_ROWS;
+        int end = offset + NUM_OF_VISIBLE_ROWS;
+        end = Math.min(end, systems.size());
+        model.addAttribute("systems", systems.subList(offset, end));
+        model.addAttribute("page", page);
+        model.addAttribute("numOfPages", numOfPages);
         return "search/systems";
     }
 }
